90aee9eda9f2c6dec339defab2d9a568dc6d3bdc
Yasin Arafat <mdyasinarafat3@gmail.com>
2021-02-09 04:25:05 +0600

    Update database(612254-yasin).sql
    added order,create pizza function

--- database(612254-yasin).sql
+++ database(612254-yasin).sql
@@ -178,9 +178,96 @@ CREATE TABLE IF NOT EXISTS public.compose_pizzas
 	cp_id serial NOT NULL UNIQUE PRIMARY KEY,
 	cp_name text,
 	cp_size integer,
-	cp_price real
+	cp_price real,
+	cp_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	cp_ingredient integer[] NOT NULL
 );
 
 
 insert into public.bakers(baker_name) values('yasin');
 select * from bakers;
+
+
+CREATE OR REPLACE FUNCTION show_ingredients_in_stock()
+	RETURNS TABLE(
+		ing_id bigint,
+		ing_name text,
+		ing_region text,
+		ing_price real,
+		ing_stock integer
+	)
+	LANGUAGE 'plpgsql'
+	AS $$
+ BEGIN
+  return query SELECT ingredient_id,ingredient_name,region,price,stock FROM public.ingredients WHERE ingredients.stock > 0;
+ END;
+$$
+SELECT show_ingredients_in_stock();
+SELECT add_ingredient('Walnut','Bavaria',0.55,0,'f',5);
+SELECT * FROM ingredients;
+
+CREATE OR REPLACE FUNCTION list_of_all_pizzas()
+	RETURNS table(
+		cp_id integer,
+		cp_name text,
+		cp_size integer,
+		cp_price real
+	)
+	LANGUAGE 'plpgsql'
+	AS $$
+ BEGIN
+  RETURN query SELECT * from public.compose_pizzas;
+ END;
+$$
+SELECT list_of_all_pizzas();
+SELECT * FROM compose_pizzas;
+INSERT INTO compose_pizzas(cp_name, cp_size, cp_price) VALUES('Veg tomato pizza',24,2.5);
+
+SELECT * FROM compose_pizzas
+ORDER BY cp_date DESC LIMIT 2;
+
+CREATE OR REPLACE FUNCTION create_pizza(pizza_name text, inid integer[],OUT pizza_name_out text,OUT total numeric(10,2))
+    LANGUAGE 'plpgsql'
+    
+AS $BODY$
+DECLARE
+   i integer;
+   restock integer= 0;
+   summ real=0;
+BEGIN
+  FOREACH i IN ARRAY inid
+   LOOP
+      RAISE NOTICE '%', i;
+	  RAISE NOTICE '%', summ;
+	  
+	  summ = summ + (SELECT price FROM public.ingredients WHERE ingredient_id = i);
+	  restock = (SELECT stock FROM public.ingredients WHERE ingredient_id = i) - 1;
+	 -- UPDATE public.ingredients SET stock = restock WHERE ingredient_id = i;
+	  
+   END LOOP;
+   RAISE NOTICE '%', summ;
+   $3 = $1;
+   $4 = summ;
+   insert into compose_pizzas(cp_name,cp_size,cp_price,cp_date,cp_ingredient) values (pizza_name,24,total,now(),inid);
+END;
+$BODY$;
+SELECT create_pizza('mixed2 pizza',array[11,9]);
+SELECT * FROM compose_pizzas;
+
+CREATE OR REPLACE FUNCTION order_pizza(IN compose_pizza_id integer,OUT line integer[])
+	LANGUAGE 'plpgsql'
+	AS $$
+ DECLARE
+  i integer;
+  restock integer= 0;
+  get_cp_ing integer[]= (SELECT cp_ingredient FROM compose_pizzas WHERE cp_id = compose_pizza_id);
+ BEGIN
+  FOREACH i IN ARRAY get_cp_ing
+  LOOP
+  	RAISE NOTICE '%', i;
+	restock = (SELECT stock FROM public.ingredients WHERE ingredient_id = i) - 1;
+	UPDATE public.ingredients SET stock = restock WHERE ingredient_id = i;
+  END LOOP;
+ END;
+$$;
+select order_pizza(6);
